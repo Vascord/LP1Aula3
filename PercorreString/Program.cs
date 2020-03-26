137c2186a6f4e494a708550656bcfdc13e997091
Vascord <duarte-vasco@outlook.com>
2020-03-26 10:53:38 +0000

    Second commit

---
+++ PercorreString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newstr = "";
+
+            Console.WriteLine("Give me a string, then a character : ");
+            string phrase = Console.ReadLine();
+            string character = Console.ReadLine();
+            char c = char.Parse(character);
+            for(int i = 0; i < phrase.Length; i++)
+            {
+                if(c == phrase[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    newstr += phrase[i];
+                }
+            }
+            Console.Write(newstr);
+        }
+    }
+}
